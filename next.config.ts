3e215d9bb523684fcbb3a536943cd816d1a421d0
classsync01 <apnafuture01@gmail.com>
2025-10-20 14:08:24 +0530

    removed pwa

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,25 @@
-const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-});
+// const withPWA = require("next-pwa")({
+//   dest: "public",
+//   register: true,
+//   skipWaiting: true,
+// });
 
-const nextConfig = withPWA({
+// const nextConfig = withPWA({
+//   reactStrictMode: true,
+//   eslint: { ignoreDuringBuilds: true },
+//   typescript: { ignoreBuildErrors: true },
+//   experimental: {
+//     appDir: true, // important for App Router
+//   },
+// });
+
+// module.exports = nextConfig;
+
+
+const nextConfig = {
   reactStrictMode: true,
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
-  experimental: {
-    appDir: true, // important for App Router
-  },
-});
+};
 
 module.exports = nextConfig;
